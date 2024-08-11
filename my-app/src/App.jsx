357ceb7dbc2f76ac6c566514debd49a573a22bdc
Larry Le <lle114@student.hbuhsd.edu>
2024-08-11 01:52:35 -0700

    implementation of front page cover

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
 import Demo from "./components/Demo";
+import Navigation from "./components/Navigation";
+
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-center mt-10">A-Frame React Demo</h1>
+    <div className="w-10/12 flex flex-col gap-20">
+      <Navigation />
 
-      <Demo />
-    </>
+      <section className="flex w-full justify-between">
+        <div className="flex flex-col gap-10 w-144">
+          <h1 className="text-prim-1 font-semibold text-5xxl">
+            Enjoy Community-made 3D Components.
+          </h1>
+          <p className="font-semibold text-prim-1 text-4xxl">
+            Built with HTML. Free for everyone.
+          </p>
+
+          <article className="flex gap-8">
+            <p className="text-2xl font-semibold text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600">
+              Explore
+            </p>
+            <p className="text-2xl font-semibold text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600">
+              Contribute
+            </p>
+          </article>
+        </div>
+
+        <Demo />
+      </section>
+    </div>
   );
 }
 
--- my-app/src/components/Demo.jsx
+++ my-app/src/components/Demo.jsx
@@ -1,19 +1,42 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 // aframe does not need to be installed because it is imported in index.html
-export default function demo() {
+export default function Demo() {
+  const [toolTip, setToolTip] = useState(false);
+
+  let changeToolTip = () => {
+    setToolTip(!toolTip);
+  };
+
+  let test = () => {
+    console.log("test");
+  };
+
   return (
-    <>
+    // we need both onMouseOver and onMouseOut to change the state as necessary
+    <div
+      className="w-1/2 h-96 relative"
+      onMouseOver={() => changeToolTip()}
+      onMouseOut={() => changeToolTip()}
+    >
       {/* the embedded property of a-scene removes the default full-screen canvas render */}
       {/* because a-frame is a web framework and not a react framework, we use class instead of className */}
-      <a-scene embedded class="w-96 h-96">
-        <a-torus position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" />
+      <a-scene embedded class="w-full h-full">
+        <a-torus position="-1 2 -3" rotation="0 45 0" color="#4CC3D9" />
 
-        <a-sky color={"#290292"} />
-        <a-entity environment="preset: forest; dressingAmount: 500"></a-entity>
+        <a-sky color="#0000FF" />
       </a-scene>
-    </>
+
+      {toolTip && (
+        <p
+          className="absolute z-1 top-3 right-3 text-white text-xl"
+          onClick={() => test()}
+        >
+          Click here to copy the code!
+        </p>
+      )}
+    </div>
   );
 }
