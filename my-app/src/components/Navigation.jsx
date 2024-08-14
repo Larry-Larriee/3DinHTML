6a134da4de2295356ef8a60e868078df1f6ded68
Larry Le <lle114@student.hbuhsd.edu>
2024-08-14 14:15:34 -0700

    navigation menu

--- my-app/src/components/Navigation.jsx
+++ my-app/src/components/Navigation.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 
 export default function Navigation() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -6,6 +7,19 @@ export default function Navigation() {
   const changeMenuOpen = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const menu = useRef();
+
+  useEffect(() => {
+    async function animateOpen() {
+      await new Promise((resolve) => setTimeout(resolve, 50));
+
+      menu.current.classList.add("opacity-100");
+      menu.current.classList.add("slide-in-from-left");
+    }
+
+    if (menuOpen) animateOpen();
+  }, [menuOpen]);
 
   return (
     <nav className="w-full border-b-2 flex gap-3 py-3 justify-between px-12 items-center bg-prim-2">
@@ -30,11 +44,59 @@ export default function Navigation() {
         />
       </svg>
 
-      {
-        // menuOpen && (
-        // Headless UI modal for the menu, which automatically renders at the top level of the DOM
-        // )
-      }
+      {menuOpen && (
+        <Dialog
+          open={menuOpen}
+          onClose={() => changeMenuOpen()}
+          className="fixed inset-0 w-screen h-screen"
+        >
+          <DialogPanel
+            className="flex flex-col gap-8 absolute bg-prim-3 left-0 w-80 h-screen z-30 opacity-0"
+            ref={menu}
+          >
+            <DialogTitle className="text-prim-1 text-4xl pt-9 pl-10 font-lemon">
+              Menu
+            </DialogTitle>
+
+            <ul className="list-none flex flex-col w-full gap-5 pl-10">
+              <li>
+                <a
+                  href="/"
+                  className="hover:scale-105 duration-200 ease-in-out transition font-league text-xl text-prim-1"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/account"
+                  className="hover:scale-105 duration-200 ease-in-out transition font-league text-xl text-prim-1"
+                >
+                  Account
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/explore"
+                  className="hover:scale-105 duration-200 ease-in-out transition font-league text-xl text-prim-1"
+                >
+                  Explore
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/contribute"
+                  className="hover:scale-105 duration-200 ease-in-out transition font-league text-xl text-prim-1"
+                >
+                  Contribute
+                </a>
+              </li>
+            </ul>
+          </DialogPanel>
+
+          <div className="inset-0 flex w-screen h-screen absolute bg-prim-half-2 z-10" />
+        </Dialog>
+      )}
     </nav>
   );
 }
