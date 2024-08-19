2ed8842066ac848837f38b19f49a223139bcea97
Larry Le <lle114@student.hbuhsd.edu>
2024-08-18 22:49:14 -0700

    account and success pages

--- my-app/src/pages/Account.jsx
+++ my-app/src/pages/Account.jsx
@@ -1,5 +1,87 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Navigation from "../components/Navigation";
 
-export default function Account() {
-  return <div>Account</div>;
+export default function Contribute() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userNameRef = useRef();
+  const passwordRef = useRef();
+
+  const changeUsername = () => {
+    setUsername(userNameRef.current.value);
+  };
+
+  const changePassword = () => {
+    setPassword(passwordRef.current.value);
+  };
+
+  const [signUp, setSignUp] = useState(false);
+  const [signIn, setSignIn] = useState(false);
+
+  useEffect(() => {
+    if (signUp) {
+      console.log("Sign Up");
+      // fetch user creation
+      setSignUp(false);
+    }
+    if (signIn) {
+      console.log("Sign In");
+      // fetch user authentication
+      setSignIn(false);
+    }
+  }, [signUp, signIn]);
+
+  return (
+    <>
+      <div className="flex w-full flex-col items-center gap-12">
+        <Navigation />
+
+        <div className="w-full px-12 flex justify-center">
+          <div className="w-full flex bg-prim-2 min-h-250 justify-center rounded-2xl">
+            <section className="w-1/3 bg-prim-3 items-center rounded-2xl flex flex-col my-10 p-10 gap-12">
+              <p className="text-5xl font-league text-prim-1">Account</p>
+
+              <div className="flex flex-col gap-1 w-full flex-none">
+                <p className="font-league text-prim-1 text-base">Username</p>
+                <input
+                  className="bg-prim-4 h-12 text-prim-1 font-league text-xl rounded-lg p-4 focus:outline-none"
+                  type="text"
+                  ref={userNameRef}
+                  onChange={() => changeUsername()}
+                />
+              </div>
+
+              <div className="flex flex-col gap-1 w-full flex-none">
+                <p className="font-league text-prim-1 text-base">Password</p>
+                <input
+                  className="bg-prim-4 h-12 text-prim-1 font-league text-xl rounded-lg p-4 focus:outline-none"
+                  type="text"
+                  ref={passwordRef}
+                  onChange={() => changePassword()}
+                />
+              </div>
+
+              <article className="flex w-full flex-col h-full justify-end">
+                <div className="flex gap-8 w-full">
+                  <p
+                    className="w-36 h-12 shadow-lg hover:cursor-pointer duration-200 ease-in-out transition hover:scale-105 hover:bg-green-700 rounded-lg bg-prim-5 text-prim-1 font-semibold text-xl leading-button text-center"
+                    onClick={() => setSignUp(true)}
+                  >
+                    Sign Up
+                  </p>
+                  <p
+                    className="w-36 h-12 shadow-lg hover:cursor-pointer duration-200 ease-in-out transition hover:scale-105 hover:bg-green-700 rounded-lg bg-prim-5 text-prim-1 font-semibold text-xl leading-button text-center"
+                    onClick={() => setSignIn(true)}
+                  >
+                    Sign In
+                  </p>
+                </div>
+              </article>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- my-app/src/pages/Success.jsx
+++ my-app/src/pages/Success.jsx
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Navigation from "../components/Navigation";
 
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col items-center gap-12">
+        <Navigation />
+
+        <div className="w-full px-12 flex justify-center">
+          <div className="w-full flex bg-prim-2 min-h-250 rounded-2xl p-12 flex-col gap-12">
+            <section className="flex flex-col gap-10 w-10/12">
+              <div className="flex gap-5 items-center">
+                <h1 className="text-5xl font-league text-prim-1">
+                  You&apos;re in!
+                </h1>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={0.75}
+                  stroke="#deded6"
+                  className="w-16 h-16"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                  />
+                </svg>
+              </div>
+
+              <p className="text-2xl font-league text-prim-1">
+                With an account, you&apos;re able to autofill you credentials
+                when contributing projects.
+              </p>
+              <p className="text-2xl font-league text-prim-1">
+                Please keep in mind that you should never reuse the same
+                password for all of your accounts. If you would like to delete
+                your account, you have the option to do so below. Otherwise, go
+                explore or contribute!
+              </p>
+            </section>
+
+            <section className="flex w-full flex-col gap-8">
+              <p className="text-xl bg-prim-5 rounded-xl shadow-xl font-semibold w-72 text-center py-4 hover:cursor-pointer hover:scale-105 transition ease-in-out duration-200 text-prim-1">
+                Delete My Account
+              </p>
+              <p className="text-xl bg-green-800 rounded-xl shadow-xl font-semibold w-72 text-center py-4 hover:cursor-pointer hover:scale-105 transition ease-in-out duration-200 text-prim-1">
+                Explore
+              </p>
+              <p className="text-xl bg-green-800 rounded-xl shadow-xl font-semibold w-72 text-center py-4 hover:cursor-pointer hover:scale-105 transition ease-in-out duration-200 text-prim-1">
+                Contribute
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
