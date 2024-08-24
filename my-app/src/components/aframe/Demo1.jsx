40422a86a7046e28e514d1b3be7fbbc55b138e1f
Larry Le <lle114@student.hbuhsd.edu>
2024-08-23 23:05:54 -0700

    light mode part one
    created light mode theme possible with tailwind and localstorage storing the theme value

--- my-app/src/components/aframe/Demo1.jsx
+++ my-app/src/components/aframe/Demo1.jsx
@@ -22,13 +22,25 @@ export default function Demo() {
     >
       {/* the embedded property of a-scene removes the default full-screen canvas render */}
 
-      <iframe
-        className="w-full h-full"
-        srcDoc={
-          header +
-          '<a-scene><a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box><a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere><a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder><a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane><a-sky color="#1B1B1F"></a-sky></a-scene>'
-        }
-      ></iframe>
+      {localStorage.getItem("theme") === "dark" && (
+        <iframe
+          className="w-full h-full"
+          srcDoc={
+            header +
+            '<a-scene><a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box><a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere><a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder><a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane><a-sky color="#1B1B1F"></a-sky></a-scene>'
+          }
+        ></iframe>
+      )}
+
+      {localStorage.getItem("theme") === "light" && (
+        <iframe
+          className="w-full h-full"
+          srcDoc={
+            header +
+            '<a-scene><a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box><a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere><a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder><a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane><a-sky color="#f9f9fa"></a-sky></a-scene>'
+          }
+        ></iframe>
+      )}
 
       {toolTip && (
         <p className="absolute z-1 -bottom-10 font-league text-prim-1 animate-pulse text-xl">
--- my-app/src/pages/Landing.jsx
+++ my-app/src/pages/Landing.jsx
@@ -1,7 +1,7 @@
+import React from "react";
+
 import Demo1 from "../components/aframe/Demo1";
 import Project from "../components/aframe/Project";
-
-import Navigation from "../components/Navigation";
 
 import aFrameLogo from "../assets/aframe-logo.png";
 import EditorUI from "../components/helper/EditorUI";
@@ -10,41 +10,68 @@ function App() {
   let openAFrame = () => {
     window.open("https://aframe.io/", "_blank");
   };
-
   let openExplore = () => {
     window.location.href = "/explore";
   };
-
   let openContribute = () => {
     window.location.href = "/contribute";
   };
 
+  // the theme should have already been set to dark by default through index.html, so we can toggle here
+  let toggleTheme = () => {
+    if (localStorage.getItem("theme") === "dark")
+      localStorage.setItem("theme", "light");
+    else localStorage.setItem("theme", "dark");
+  };
+
   return (
-    <div className="flex w-full items-center flex-col gap-20">
-      <nav className="w-10/12 flex gap-3 pt-10 items-center">
-        <h1 className="text-prim-1 text-6xl font-lemon">3D</h1>
-        <p className="text-prim-1 text-3xl font-league">in HTML</p>
+    <div className="flex w-full items-center gap-20 flex-col">
+      <nav className="w-10/12 flex pt-10 justify-between items-center">
+        <div className="flex gap-3 items-center">
+          <h1 className="text-prim-2 dark:text-prim-1 text-6xl font-lemon">
+            3D
+          </h1>
+          <p className="text-prim-2 dark:text-prim-1 text-3xl font-league">
+            in HTML
+          </p>
+        </div>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="w-12 h-12 hover:cursor-pointer hover:rotate-180 text-prim-2 duration-1000 transition ease-in-out dark:text-white hover:text-yellow-400"
+          onClick={() => toggleTheme()}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg>
       </nav>
 
       <section className="w-10/12 flex justify-between mb-24">
         <div className="flex flex-col gap-10 w-144">
-          <h1 className="text-prim-1 font-bold xl:font-semibold text-4xl xl:text-5xxl">
+          <h1 className="text-prim-2 dark:text-prim-1 font-bold xl:font-semibold text-4xl xl:text-5xxl">
             Enjoy Community-made 3D Components.
           </h1>
-          <p className="font-bold xl:font-semibold text-prim-1 text-3xl xl:text-4xxl">
+          <p className="font-bold xl:font-semibold text-prim-2 dark:text-prim-1 text-3xl xl:text-4xxl">
             Built with HTML. Free for everyone.
           </p>
 
           <article className="flex gap-8">
             <p
               onClick={openExplore}
-              className="text-2xl font-semibold text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
+              className="text-2xl font-semibold text-white dark:text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
             >
               Explore
             </p>
             <p
               onClick={openContribute}
-              className="text-2xl font-semibold text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
+              className="text-2xl font-semibold text-white dark:text-prim-1 rounded-xl py-2 px-5 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
             >
               Contribute
             </p>
@@ -54,12 +81,12 @@ function App() {
         <Demo1 />
       </section>
 
-      <section className="flex w-full justify-center xl:mb-12 flex-col gap-16 xl:gap-28 items-center py-16 bg-prim-2">
+      <section className="flex w-full justify-center xl:mb-12 flex-col gap-16 xl:gap-28 items-center py-16 bg-sec-1 dark:bg-prim-2">
         <article className="flex flex-col w-10/12 gap-5">
-          <h1 className="text-prim-1 text-center font-semibold text-5xl xl:text-5xxl">
+          <h1 className="text-prim-2 dark:text-prim-1 text-center font-semibold text-5xl xl:text-5xxl">
             Hundreds of projects
           </h1>
-          <p className="text-center font-semibold text-prim-1 text-2xl xl:text-3xxl">
+          <p className="text-center font-semibold text-prim-2 dark:text-prim-1 text-2xl xl:text-3xxl">
             Copy to clipboard. Paste in your website.
           </p>
         </article>
@@ -100,10 +127,10 @@ function App() {
           />
 
           <article className="flex flex-col gap-5 w-full lg:144 xl:w-144 order-1 lg:order-2">
-            <h1 className="font-semibold text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
+            <h1 className="font-semibold text-prim-2 dark:text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
               Want Fullscreen?
             </h1>
-            <p className="font-semibold text-prim-1 text-2xl xl:text-3xxl leading-sm">
+            <p className="font-semibold text-prim-2 dark:text-prim-1 text-2xl xl:text-3xxl leading-sm">
               Remove embeded from your code. As simple as that.
             </p>
           </article>
@@ -111,10 +138,10 @@ function App() {
 
         <div className="flex flex-col lg:flex-row justify-between gap-10 xl:gap-10 w-10/12 items-center">
           <article className="flex flex-col gap-5 w-full  lg:w-144 xl:w-192">
-            <h1 className="font-semibold text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
+            <h1 className="font-semibold text-prim-2 dark:text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
               What about modifying the 3D?
             </h1>
-            <p className="font-semibold text-prim-1 text-2xl xl:text-3xxl leading-sm w-10/12">
+            <p className="font-semibold text-prim-2 dark:text-prim-1 text-2xl xl:text-3xxl leading-sm w-10/12">
               Our projects are mostly made with A-Frame. Learn more{" "}
               <span
                 onClick={() => openAFrame()}
@@ -143,10 +170,10 @@ function App() {
           />
 
           <article className="flex flex-col gap-5 order-1 lg:order-2 w-full lg:w-144 xl:w-144">
-            <h1 className="font-semibold text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
+            <h1 className="font-semibold text-prim-2 dark:text-prim-1 text-5xl lg:text-4xl xl:text-5xxl">
               I want to share my work!
             </h1>
-            <p className="font-semibold text-prim-1 text-2xl xl:text-3xxl leading-sm w-11/12">
+            <p className="font-semibold text-prim-2 dark:text-prim-1 text-2xl xl:text-3xxl leading-sm w-11/12">
               It&apos;s nothing more than pasting your code, starting at{" "}
               <span className="text-green-500">&lt;a-scene&gt;</span>
             </p>
@@ -155,20 +182,20 @@ function App() {
       </section>
 
       <section className="flex flex-col items-center gap-5 mb-16 mt-8">
-        <p className="text-prim-1 text-2xl xl:text-4xxl font-semibold">
+        <p className="text-prim-2 dark:text-prim-1 text-2xl xl:text-4xxl font-semibold">
           that&apos;s enough reading lol.
         </p>
 
         <article className="flex w-full justify-between">
           <p
             onClick={openExplore}
-            className="xl:text-2xxl font-semibold text-prim-1 rounded-xl py-2 px-4 text-xl xl:py-3 xl:px-10 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
+            className="xl:text-2xxl font-semibold text-white dark:text-prim-1 rounded-xl py-2 px-4 text-xl xl:py-3 xl:px-10 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
           >
             Explore
           </p>
           <p
             onClick={openContribute}
-            className="xl:text-2xxl font-semibold text-prim-1 rounded-xl py-2 px-4 text-xl xl:py-3 xl:px-10 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
+            className="xl:text-2xxl font-semibold text-white dark:text-prim-1 rounded-xl py-2 px-4 text-xl xl:py-3 xl:px-10 hover:cursor-pointer hover:scale-105 duration-200 transition ease-in-out bg-orange-600"
           >
             Contribute
           </p>
